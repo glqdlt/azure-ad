546b3c087bc72f9321ae3763601f093e50cffd74
전일웅 <jhun@nm-4ones.com>
2018-10-10 10:35:07 +0900

    git init

---
+++ src/main/java/com/glqdlt/ex/aadspringboot/AadSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.ex.aadspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AadSpringBootApplication.class, args);
+    }
+}
